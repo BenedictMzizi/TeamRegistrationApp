4c4777df86e8fd1942b20e558594ffef442a726f
BenedictMzizi <61235432+BenedictMzizi@users.noreply.github.com>
2025-06-04 22:35:14 +0200

    Update supabaseClient.js

--- TeamRegistrationApp/frontend/src/supabaseClient.js
+++ TeamRegistrationApp/frontend/src/supabaseClient.js
@@ -1,6 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://avkvqrtxwqyugzoekoru.supabase.co'; // Your project URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';  // Your anon public key
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://avkvqrtxwqyugzoekoru.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
