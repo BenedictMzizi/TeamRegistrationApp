45d9c57be289dafb3f5ed9ef267887cb13c1aeb1
BenedictMzizi <61235432+BenedictMzizi@users.noreply.github.com>
2025-06-08 22:11:14 +0200

    Update supabaseClient.js

--- TeamRegistrationApp/frontend/src/supabaseClient.js
+++ TeamRegistrationApp/frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://avkvqrtxwqyuqzoekoru.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2a3ZxcnR4d3F5dWd6b2Vrb3J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDMzNTgsImV4cCI6MjA2NDYxOTM1OH0.Xb9j0xPN196l25MZLoqTWGZPXwm4P0hCXQi8PBo6Ws8';
+const supabaseUrl = 'https://avkvqrtxwqyuugzoekoru.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2a3ZxcnR4d3F5dWd6b2Vrb3J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDMzNTgsImV4cCI6MjA2NDYxOTM1OH0.Xb9j0xPN196l25MZLoqTWGZPXwm4P0hCXQi8PBo6Ws8';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
