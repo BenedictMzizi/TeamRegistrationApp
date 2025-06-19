7b5266fb9808ec5fc2f22b6968b9d9b712ccbd9d
BenedictMzizi <61235432+BenedictMzizi@users.noreply.github.com>
2025-06-19 21:42:14 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,3 +4,10 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+
+//import { createClient } from '@supabase/supabase-js'
+//const supabaseUrl = 'https://avkvqrtxwqyugzoekoru.supabase.co'
+//const supabaseKey = process.env.SUPABASE_KEY
+//const supabase = createClient(supabaseUrl, supabaseKey)
